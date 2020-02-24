c5c75ae90390c9a2e87fbafc3eebe69c6fd4bfa5
[stefanhallberg123] <[stefanhalllberg@live.se]>
2020-02-24 12:36:21 +0100

    ändringar för att fungera med netflify test

--- dist/index.js
+++ dist/index.js
@@ -24,6 +24,14 @@ var _bodyParser = require("body-parser");
 
 var _bodyParser2 = _interopRequireDefault(_bodyParser);
 
+var _fs = require("fs");
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _ejs = require("ejs");
+
+var _ejs2 = _interopRequireDefault(_ejs);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
@@ -48,4 +56,36 @@ var options = {
 _mongoose2.default.connect(_config2.default.databaseURL, options).then(function () {
   app.listen(port);
   console.log("listening on " + port + " the coolest port in the world");
+});
+
+function ejs2html(_ref) {
+  var path = _ref.path,
+      outPath = _ref.outPath,
+      data = _ref.data,
+      options = _ref.options;
+
+  _fs2.default.readFile(path, "utf8", function (err, data) {
+    if (err) {
+      console.log(err);
+      return false;
+    }
+    _ejs2.default.renderFile(path, data, options, function (err, html) {
+      if (err) {
+        console.log(err);
+        return false;
+      }
+      _fs2.default.writeFile(outPath, html, function (err) {
+        if (err) {
+          console.log(err);
+          return false;
+        }
+        return true;
+      });
+    });
+  });
+}
+
+ejs2html({
+  path: __dirname + "/views/todolist.ejs",
+  outPath: __dirname + "/public/index.html"
 });
